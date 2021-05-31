63e3e07f5487485bbfee376cd2a9fe6df1c04aa8
miamollie <mia.deburca@99designs.com>
2021-05-31 13:02:52 +1000

    Style mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from "react";
 import styled from "styled-components/macro";
 import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from "../../constants";
+import { COLORS } from "../../constants";
 
 import IconButton from "../IconButton";
 
@@ -13,23 +13,86 @@ const MobileMenu = ({ isOpen, onDismiss, onOpen }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <DialogWrapper>
+      <DialogContentWrapper>
+        <CloseButtonWrapper>
+          <IconButton label="Dismiss menu" onClick={onDismiss} icon="close" />
+        </CloseButtonWrapper>
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </DialogContentWrapper>
+    </DialogWrapper>
   );
 };
 
 export default MobileMenu;
+
+const FooterLink = styled.a`
+  text-decoration: none;
+  color: ${COLORS.gray[700]};
+  font-size: 16px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const NavLink = styled.a`
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-size: 18px;
+
+  &:hover {
+    color: ${COLORS.secondary};
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+const DialogContentWrapper = styled(DialogContent)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  background-color: white;
+  width: min(20rem, 80vw);
+  float: right;
+  padding: 32px 22px;
+`;
+
+const CloseButtonWrapper = styled.div`
+  align-self: flex-end;
+`;
+
+const DialogWrapper = styled(DialogOverlay)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  background: hsla(220, 5%, 40%, 0.8);
+`;
